5370aed3ab66028db45508b9d527272afc32f30f
dhhh30 <chendylan680@gmail.com>
2023-09-09 17:05:31 +0800

    removed asset static file service and added templating support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use actix_web::middleware::Logger;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpResponseBuilder};
 use diesel::r2d2::ConnectionManager;
 use diesel::r2d2::Pool;
 use diesel::sqlite::SqliteConnection;
 use env_logger::Env;
 use host::database_handler::*;
-use std::fs;
+use host::templating::*;
 //state passed to handlers
 struct State {
     db_pool: Pool<ConnectionManager<SqliteConnection>>,
@@ -18,10 +18,15 @@ async fn index() -> impl Responder {
     unwrap();
     HttpResponse::Ok().body(html)
 }
-
+//new alternative index
+#[get("/new/index")]
+async fn index_new() -> impl Responder {
+    let index_str = index_template();
+    let response = HttpResponseBuilder::new(200);
+    response.body(index_str)
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_files as fs;
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
@@ -30,12 +35,11 @@ async fn main() -> std::io::Result<()> {
                 db_pool: acquire_connection_pool(),
                 auth_db_pool: acquire_authentication_pool(),
             }))
-            .service(fs::Files::new("/assets", "./assets"))
             .service(index)
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
---
+++ src/templating.rs
@@ -0,0 +1,13 @@
+use askama::Template;
+
+//template struct for index
+#[derive(Template)]
+#[template(path="index.html")]
+struct IndexTemplate<'a> {
+    name :&'a str,
+}
+//templating for index
+pub fn index_template() -> String{
+    let index = IndexTemplate{name:"hell"};
+    index.render().unwrap()
+}
